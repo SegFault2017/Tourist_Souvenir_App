6de2b6682453338bdc13a9bcde71ba1b0daace55
Raymond Tan <ray.tangent42@gmail.com>
2021-02-08 15:51:25 -0500

    Deleted route file.

--- backend/src/router/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var routes_1 = require("./../api/logEntry/routes");
-exports["default"] = (function (app) {
-    app.use("/api/logs", routes_1["default"]);
-});
